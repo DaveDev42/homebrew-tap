892c6c1c001c30e0b17432570ccdade57c7a1970
Dave Choi <dave.dev@icloud.com>
2026-05-06 12:12:28 +0900

    macstate 0.0.11

--- Formula/macstate.rb
+++ Formula/macstate.rb
@@ -1,14 +1,14 @@
 class Macstate < Formula
   desc "CLI exposing macOS system signals (network, power) as JSON"
   homepage "https://github.com/DaveDev42/macstate"
-  version "0.0.4"
+  version "0.0.11"
   license "MIT"
 
   depends_on arch: :arm64
   depends_on :macos
 
-  url "https://github.com/DaveDev42/macstate/releases/download/v#{version}/macstate-v#{version}-aarch64-apple-darwin.tar.gz"
-  sha256 "6c4253962a25979f717f5c9ec9319801d361c1c96ba44561b93a5cd879367d6d"
+  url "https://github.com/DaveDev42/macstate/releases/download/v0.0.11/macstate-v0.0.11-aarch64-apple-darwin.tar.gz"
+  sha256 "031c7a199e6c30027651483c88d2b6b5753a13332a52efe0eed6a22ce8b0ea4d"
 
   def install
     bin.install "macstate"
