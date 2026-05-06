8e3a43d1c0a021ab8cb7fb6db31fbe8d8a864ff5
Dave Choi <dave.dev@icloud.com>
2026-05-07 02:52:06 +0900

    git-worktree-manager 0.1.5

--- Formula/git-worktree-manager.rb
+++ Formula/git-worktree-manager.rb
@@ -1,14 +1,14 @@
 class GitWorktreeManager < Formula
   desc "CLI tool integrating git worktree with AI coding assistants"
   homepage "https://github.com/DaveDev42/git-worktree-manager"
-  version "0.1.4"
+  version "0.1.5"
   license "BSD-3-Clause"
 
   depends_on arch: :arm64
   depends_on :macos
 
-  url "https://github.com/DaveDev42/git-worktree-manager/releases/download/v0.1.4/gw-aarch64-apple-darwin.tar.gz"
-  sha256 "6d8dede4907e9fd490069164e8a440425d173e20971ca290b908d64cf97c2b56"
+  url "https://github.com/DaveDev42/git-worktree-manager/releases/download/v0.1.5/gw-aarch64-apple-darwin.tar.gz"
+  sha256 "3f85e7fefdf05a324b8c8b2daa540cd386091f4eff990f9e4d973b6718ef2c48"
 
   def install
     bin.install "gw"
